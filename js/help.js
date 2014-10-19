2b2967444d421a79b1fd6ab3967994b4b9f40c07
fabian <f.pena.jacobo@gmail.com>
2014-10-19 13:25:44 -0500

    Ordenada distribución de código

--- js/help.js
+++ js/help.js
@@ -1,16 +1,89 @@
-$(document).ready(
-	function(){
-		$("#lexema").click(
-			function(){
-				text = $(this).val();
-				$("#nombre").val(text);
-			}
-		);
-		$("#nombre").click(
-			function(){
-				text = $(this).val();
-				$("#lexema").val(text);
-			}
-		);
-	}
-);
+function relationLists(list1,list2){
+	$(list1).click(
+		function(){
+			text = $(this).val();
+			$(list2).val(text);
+		}
+	);
+	$(list2).click(
+		function(){
+			text = $(this).val();
+			$(list1).val(text);
+		}
+	);
+}
+
+//obtener palabras reservadas
+
+function isIdentificator(value){
+	return false;
+}
+
+function getReservedWord(key){
+	var array = {
+		'alias' : 'mo',
+		'break' : 'me',
+		//Operadores lógicos
+		'and' : 'ol',
+		'or':'ol',
+		'not':'ol',
+		'&&' : 'ol',
+		'||':'ol',
+
+		
+		//Operadores Aritmeticos
+		'+':'oa',
+		'-':'oa',
+		'*':'oa',
+		'/':'oa',
+		'>':'oa',
+		'<':'oa',
+        //Operadores modificador 
+        'int':'td',
+		'float':'td',
+		'decimal':'td',
+		'double':'td',
+		'date':'td',
+		'datetime':'td'
+
+
+	};
+	return array[key];
+}
+
+function getType(key){
+	var array = {
+		'ol' : 'operador lógico',
+		'oa' : 'operador aritmético',
+		'mo' : 'modificador',
+		'me' : 'método',
+		'td' : 'tipo de dato'
+	};
+	return array[key];
+}
+
+//Obtener elementos léxicos
+//
+
+function getElement(key){
+	var array = {
+		'=' : 'igual',
+		'"' : 'comilla doble',
+		"'" : 'comilla simple',
+		'.' : 'punto',
+		',' : 'coma',
+		';' : 'punto y coma',
+		':' : 'dos punto',
+		'[' : 'abrir corchete',
+		']' : 'cerrar corchete',
+		'{' : 'abrir llave',
+		'}' : 'cerrar llave',
+		'(' : 'abrir parentesis',
+		')' : 'cerrar paretensis',
+		'-' : 'guion',
+		'_' : 'guion abajo'
+
+	};
+	return array[key];
+}
+
--- js/index.js
+++ js/index.js
@@ -2,15 +2,8 @@
 //función principal
 $(document).ready(function()
 {
-  var x;
-  x=$("#a");
-
-  x.click(function(){
-  		alert('Esto pasa cuando te metes con chibolas.');
-	});
-
-  y = $("#textid")
-  y.on('change keyup paste' ,function(){
+  relationLists("#nombre", "#lexema");
+  $("#textid").on('change keyup paste', function(){
   		value = $(this).attr('value');
   		result = $("#result")
   		if (getReservedWord(value)) {
@@ -34,78 +27,4 @@ $(document).ready(function()
   		};   		
 	});
 });
-
-//obtener palabras reservadas
-
-function isIdentificator(value){
-	return false;
-}
-
-function getReservedWord(key){
-	var array = {
-		'alias' : 'mo',
-		'break' : 'me',
-		//Operadores lógicos
-		'and' : 'ol',
-		'or':'ol',
-		'not':'ol',
-		'&&' : 'ol',
-		'||':'ol',
-
-		
-		//Operadores Aritmeticos
-		'+':'oa',
-		'-':'oa',
-		'*':'oa',
-		'/':'oa',
-		'>':'oa',
-		'<':'oa',
-        //Operadores modificador 
-        'int':'td',
-		'float':'td',
-		'decimal':'td',
-		'double':'td',
-		'date':'td',
-		'datetime':'td'
-
-
-	};
-	return array[key];
-}
-
-function getType(key){
-	var array = {
-		'ol' : 'operador lógico',
-		'oa' : 'operador aritmético',
-		'mo' : 'modificador',
-		'me' : 'método',
-		'td' : 'tipo de dato'
-	};
-	return array[key];
-}
-
-//Obtener elementos léxicos
-//
-
-function getElement(key){
-	var array = {
-		'=' : 'igual',
-		'"' : 'comilla doble',
-		"'" : 'comilla simple',
-		'.' : 'punto',
-		',' : 'coma',
-		';' : 'punto y coma',
-		':' : 'dos punto',
-		'[' : 'abrir corchete',
-		']' : 'cerrar corchete',
-		'{' : 'abrir llave',
-		'}' : 'cerrar llave',
-		'(' : 'abrir parentesis',
-		')' : 'cerrar paretensis',
-		'-' : 'guion',
-		'_' : 'guion abajo'
-
-	};
-	return array[key];
-}
 
